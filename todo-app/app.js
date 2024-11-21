b5bb7ef03887db68f4bc462bd62cb974f8f48417
root <root@ip-10-0-19-194.us-west-2.compute.internal>
2024-11-21 17:04:56 +0000

    first commit

---
+++ todo-app/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+const tasks = [];
+
+app.get("/", (req, res) => {
+  res.render("index", { tasks });
+});
+
+app.post("/add", (req, res) => {
+  const newTask = req.body.task;
+  tasks.push(newTask);
+  res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
